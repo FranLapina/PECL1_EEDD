4deee33f18a747d8a6a6452fddb9f74b15af5117
LuisIglesiasP <61018910+LuisIglesiasP@users.noreply.github.com>
2021-10-22 13:40:22 +0200

    Update utilidades.h

--- PECL1/include/utilidades.h
+++ PECL1/include/utilidades.h
@@ -12,6 +12,29 @@ class utilidades
 
     private:
 };
+
+/*
+    CLASE ID
+*/
+
+class id
+{
+    struct Identificador
+    {
+        char ID[7];
+        string latitud
+
+    };
+    struct coordenadas
+    {
+        int lat_max(-3190);
+        int lat_min(-3250);
+        int lon_max(40310);
+        int lon_min(40280);
+        int longitud(lon_min + rand() % (lon_max+1 - lon_min));
+        int latitud (lat_min + rand() % (lat_max+1 - lat_min));
+    };
+};
 /*
     CLASE NODO
 */
