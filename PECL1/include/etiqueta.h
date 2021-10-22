cfbb08b31ffdf75506474be78d5fdd42e4b15a49
FranLapina <92981873+FranLapina@users.noreply.github.com>
2021-10-22 12:14:05 +0200

    Add files via upload

---
+++ PECL1/include/etiqueta.h
@@ -0,0 +1,16 @@
+#ifndef ETIQUETA_H
+#define ETIQUETA_H
+
+
+class etiqueta
+{
+    public:
+        etiqueta();
+        virtual ~etiqueta();
+
+    protected:
+
+    private:
+};
+
+#endif // ETIQUETA_H
